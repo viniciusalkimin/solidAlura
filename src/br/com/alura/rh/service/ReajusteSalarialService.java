a8d6328f7adcf3350379f62c5a9d595e10844939
viniciusalkimin <viniciusg_alkimin@hotmail.com>
2023-05-31 19:23:29 -0300

    Aplicando SRP - Delegando responsabilidade de validar o aumento salarial para classe service, aumentando a coesão.

---
+++ src/br/com/alura/rh/service/ReajusteSalarialService.java
@@ -0,0 +1,19 @@
+package br.com.alura.rh.service;
+
+import br.com.alura.rh.ValidacaoException;
+import br.com.alura.rh.model.Funcionario;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+public class ReajusteSalarialService {
+
+    public void reajustarSalarioDoFuncionario(Funcionario funcionario, BigDecimal aumento) {
+        BigDecimal percentualReajuste = aumento.divide(funcionario.getSalario(), RoundingMode.HALF_UP);
+        if (percentualReajuste.compareTo(new BigDecimal("0.4")) > 0) {
+            throw new ValidacaoException("Reajuste nao pode ser superior a 40% do salario!");
+        }
+        BigDecimal novoSalario = funcionario.getSalario().add(aumento);
+        funcionario.atualizarSalario(novoSalario);
+    }
+}
